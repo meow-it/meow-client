74321a079143af1cb3d5b9310a8a927eca91be33
tharunoptimus <tharunoptimus@outlook.com>
2022-02-01 22:53:06 +0530

    Removed unwanted functions

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -212,37 +212,3 @@ function getCurrentPosition() {
         )
     })
 }
-
-function getLocation() {
-	console.log("getLocation was called")
-	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(showPosition, positionError)
-	} else {
-		hideLoadingDiv()
-		console.log("Geolocation is not supported by this device")
-	}
-}
-
-function positionError() {
-	console.log("Geolocation is not enabled. Please enable to use this feature")
-
-	if (allowGeoRecall && countLocationAttempts < 5) {
-		countLocationAttempts += 1
-		getLocation()
-	}
-}
-
-function showPosition() {
-	console.log("posititon accepted")
-	allowGeoRecall = false
-}
-
-function checkGeoPermission() {
-    navigator.permissions.query({name: 'geolocation'}).then(function(result) {
-        if(result.state == 'denied' || result.state == 'prompt') {
-            console.log(result)
-
-            getLocation()
-        }
-    });
-}
