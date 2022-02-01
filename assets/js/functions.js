a77edf872161282b26ec69137e86c09b64a089fd
tharunoptimus <tharunoptimus@outlook.com>
2022-02-01 16:27:03 +0530

    Formatting

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,38 +1,39 @@
 let serverURLAPIEndpoint = `http://localhost:3003/api/`
 
 async function getLocalForage(key) {
-
-    try {
-        let value = await localforage.getItem(key);
-        return value;
-    } catch (err) {
-        console.log("Something Happened: 😓", err);
-        return null
-    }
+	try {
+		let value = await localforage.getItem(key)
+		return value
+	} catch (err) {
+		console.log("Something Happened: 😓", err)
+		return null
+	}
 }
 
 async function setLocalForage(key, value) {
-    try {
-        await localforage.setItem(key, value);
-    } catch (err) {
-        console.log("Something Happened: 😓", err);
-    }
+	try {
+		await localforage.setItem(key, value)
+	} catch (err) {
+		console.log("Something Happened: 😓", err)
+	}
 }
 
 async function register() {
-    try {
-        let response = await fetch(serverURLAPIEndpoint + `register`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({})
-        })
-        let responseJSON = await response.json();
-        setLocalForage("user", responseJSON);
-        return responseJSON;
-    } catch (err) {
-        console.log("Something Happened: 😓", err);
-        return null
+	try {
+		let response = await fetch(serverURLAPIEndpoint + `register`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({}),
+		})
+		let responseJSON = await response.json()
+		setLocalForage("user", responseJSON)
+		return responseJSON
+	} catch (err) {
+		console.log("Something Happened: 😓", err)
+		return null
+	}
+}
     }
 }
